d08dc880212591d2aa378a6d72154bad6d1b12ec
samcoswymysl <rafalkrukowskipraca123@gmail.com>
2022-05-12 18:43:36 +0200

    Add global errors handling

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import 'express-async-errors';
+import { handleError} from './utils/errors';
 
 const PORT = 5000;
 
@@ -10,5 +11,15 @@ app.use(cors({
   origin: 'http://localhost:3000',
 }));
 app.use(express.json());
+
+//Routers
+
+app.get('/', async (_req, _res, _next)=> {
+
+
+
+})
+
+app.use(handleError)
 
 app.listen(PORT, 'localhost', () => console.log('Server listen on http://localhost:5000'));
